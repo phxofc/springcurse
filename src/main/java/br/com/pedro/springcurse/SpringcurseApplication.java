a9ac1a0c13ca7a84b8238fcfd071f6bc5e08c0ff
phxofc <phdfrocha99@gmail.com>
2023-11-28 10:29:31 -0300

    upando projeto stack

---
+++ src/main/java/br/com/pedro/springcurse/SpringcurseApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.springcurse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcurseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcurseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedro/springcurse/SpringcurseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.springcurse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcurseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
