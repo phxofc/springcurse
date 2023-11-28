dfcc43679ac2d44cd320b25f264acbc3c679c292
phxofc <phdfrocha99@gmail.com>
2023-11-28 10:52:21 -0300

    adicionando conversor de string para double e verificação de se o numero passando por o usuario foi letra ou numero mesmo

--- src/main/java/br/com/pedro/springcurse/MathController.java
+++ src/main/java/br/com/pedro/springcurse/MathController.java
@@ -11,11 +11,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class MathController {
     
     @GetMapping("/sum/{n1}/{n2}")
-    public Double sum (@PathVariable(value = "n1") String n1, @PathVariable(value = "n2") String n2){
+    public Double sum (@PathVariable(value = "n1") String n1, @PathVariable(value = "n2") String n2) throws Exception{
 
-        
+        if(!isNumeric(n1) || !isNumeric(n2)){
+            throw new Exception("NÃO É UM NUMERO");
+        }
 
-        return 1D;
+        return convertToDouble(n1) +convertToDouble(n2) ;
+    }
+
+    private Double convertToDouble(String strNumber) {
+
+        if(strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        //adicionando regex
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 
 }
