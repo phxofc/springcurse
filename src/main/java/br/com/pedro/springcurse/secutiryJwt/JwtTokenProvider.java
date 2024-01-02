8f5051dbc62a5d435ef2d14ef735fa75b1d03ebf
phxofc <phdfrocha99@gmail.com>
2024-01-02 08:49:02 -0300

    Finalizando a Implementação do JwtTokenProvider

--- src/main/java/br/com/pedro/springcurse/secutiryJwt/JwtTokenProvider.java
+++ src/main/java/br/com/pedro/springcurse/secutiryJwt/JwtTokenProvider.java
@@ -1,12 +1,21 @@
 package br.com.pedro.springcurse.secutiryJwt;
 
 import br.com.pedro.springcurse.data.vo.security.TokkenVO;
+import br.com.pedro.springcurse.exceptions.InvalidJwtAuthenticationExceptionException;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.Base64;
 import java.util.Date;
@@ -36,16 +45,77 @@ public class JwtTokenProvider {
     }
     public TokkenVO createAccessToken(String username, List<String> roles){
         Date now  = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliSeconds) ;
-        var accessToken = getAcessToken(username, roles, now, validity);
+        Date validity = new Date(now.getTime() + validityInMilliSeconds);
+        var accessToken = getAccessToken(username, roles, now, validity);
         var refreshToken = getRefreshToken(username, roles, now);
         return  new TokkenVO(username, true, now, validity, accessToken, refreshToken);
     }
 
-    private String getRefreshToken(String username, List<String> roles, Date now) {
+
+
+    private String getAccessToken(String username, List<String> roles, Date now, Date validity) {
+
+        String issuriUrl = ServletUriComponentsBuilder
+                .fromCurrentContextPath().build().toUriString();
+        return JWT.create()
+                .withClaim("roles", roles)
+                .withIssuedAt(now)
+                .withExpiresAt(validity)
+                .withSubject(username)
+                .withIssuer(issuriUrl)
+                .sign(algorithm)
+                .strip();
     }
 
-    private String getAcessToken(String username, List<String> roles, Date now, Date validity) {
+    private String getRefreshToken(String username, List<String> roles, Date now) {
+
+        Date validityRefreshToken  =  new Date(now.getTime() + (validityInMilliSeconds * 3));
+
+        return JWT.create()
+                .withClaim("roles", roles)
+                .withIssuedAt(now)
+                .withExpiresAt(validityRefreshToken)
+                .withSubject(username)
+                .sign(algorithm)
+                .strip();
+
     }
-    
+
+    public Authentication getAuthentication(String token){
+        DecodedJWT decodedJWT = decodedToken(token);
+        UserDetails userDetails = this.userDetailsService
+                .loadUserByUsername(decodedJWT.getSubject());
+
+        return new UsernamePasswordAuthenticationToken(userDetails,"", userDetails.getAuthorities());
+
+    }
+
+    private DecodedJWT decodedToken(String token) {
+        Algorithm  alg = Algorithm.HMAC256(secretKey.getBytes());
+        JWTVerifier verifier = JWT.require(alg).build();
+        DecodedJWT decodedJWT = verifier.verify(token);
+        return  decodedJWT;
+    }
+
+    public String resolveToken(HttpServletRequest req ){
+
+        String bearerToken = req.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return  bearerToken.substring("Bearer ".length());
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token){
+        DecodedJWT decodedJWT = decodedToken(token);
+        try {
+            if (decodedJWT.getExpiresAt().before(new Date())){
+                return false;
+            }
+            return true;
+        }catch (Exception e){
+            throw  new InvalidJwtAuthenticationExceptionException("Expired or ivalid JWT TOKEN!");
+        }
+    }
+
 }
